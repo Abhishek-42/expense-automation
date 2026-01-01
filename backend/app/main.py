b368e9367d27bca40111d021937c0aedbfe862e0
Abhishek-42 <abhishek2005abhi@gmail.com>
2026-01-01 14:07:03 +0530

    fixes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from app.db import s3_client, bucket_name  , transactions_table
 import csv
 import io
 import uuid
 from decimal import Decimal as dec
 
-
-
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload/transactions")
 async def upload_transactions(file: UploadFile = File(...)):
@@ -31,8 +37,8 @@ async def upload_transactions(file: UploadFile = File(...)):
         #For Dynamo DB value insertion 
         item ={
             "transaction_id" : str(uuid.uuid4()),
-            "User-id" : "test_user",
-            "Name-of-service" : row["description"],
+            "User_id" : "test_user",
+            "Service_Name" : row["description"],
             "amount" : dec(row["amount"]),
             "date" : row["date"]
         }
@@ -55,3 +61,9 @@ async def upload_transactions(file: UploadFile = File(...)):
 def get_transactions():
     response = transactions_table.scan()
     return {"items": response["Items"]}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
